ede1f8d12561501b62c92f0fb65c1a51d94e2805
Ana Claudia <anac.almeidasouza@gmail.com>
2021-06-24 20:27:34 -0300

    Atualizacao-servidor

--- meu-primeiro-servidor/server.js
+++ meu-primeiro-servidor/server.js
@@ -1,12 +1,27 @@
-const express = require('express')
-const app = express()
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+const express = require("express") //importando o express
+const app = express() //trazendo e armazenando a função na variavél
+
+app.get("/",(request,response)=>{
+    response.status(200).json("salve! <3")
 })
 
-app.get("/", (request, response) => {
-    response.status(200).json([{
-        "nome": "oiiiiiiiii"
-    }])
+app.get("/jurema", (request, response) => {
+    console.log('Url Jurema foi requerida')
+    response.status(200).json([
+        {
+            'nome': 'jurema',
+            'idade': 'adulta'
+        },
+        {
+            'dona': 'vitoria',
+            'tempo': '4 meses'
+        }
+    ])
 })
+
+
+app.listen(8080, () => {
+    console.log("hello")
+    console.log("Meu primeiro servidor na porta 3000")
+})  //criando a porta do servidor
